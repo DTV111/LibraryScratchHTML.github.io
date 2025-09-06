7c796813a19e9bf0eea6ec8c74cc7312a3116405
DTV111 <153782863+DTV111@users.noreply.github.com>
2025-09-06 17:16:31 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("downloadBtn").addEventListener("click", function() {
-  const link = document.createElement("a");
-  link.href = "example.pdf"; // your file
-  link.download = "example.pdf";
-  link.click();
-});
